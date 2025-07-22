9a29341f6a98bfb9c5e39de924bc65c83599e543
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-07-23 05:35:58 +0700

    feat: add signIn and signUp components with initial structure

---
+++ frontend/app/auth/signIn.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function signIn () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ frontend/app/auth/signUp.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function signUp () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
