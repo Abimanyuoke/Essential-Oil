c83fb3d4069f2c4a3283cb411e33fb43ddfcadd5
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-07-24 06:57:23 +0700

    `Refactor AboutLetter component: update styles and layout`

--- frontend/app/components/AboutLetter.tsx
+++ frontend/app/components/AboutLetter.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export default function AboutLetter() {
     return (
-        <div className='w-4/5 mx-auto'>
-            <div className="grid grid-cols-8 bg-black text-white p-14 rounded-lg shadow-lg items-center">
+        <div className='w-4/5 mx-auto py-10'>
+            <div className="grid lg:grid-cols-8 gap-10 md:gap-20 bg-black text-white p-14 rounded-xl items-center shadow-xl cursor-pointer">
                 <div className='col-span-4'>
                     <h1 className='text-4xl/normal'>What Does Cinnamon Do You To Face?</h1>
                 </div>
@@ -13,8 +13,8 @@ export default function AboutLetter() {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quis ratione ex eius quod esse nesciunt architecto ea explicabo aut eveniet, accusamus minus perspiciatis alias voluptas ab eligendi molestiae reprehenderit?</p>
                 </div>
                 <div className='col-span-1'>
-                    <div className='p-3 bg-primary rounded-full hover:bg-black hover:scale-105 hover:transition-all duration-300 cursor-pointer border-2'>
-                        <ArrowRight className='text-' size={40} />
+                    <div className='p-1 bg-primary rounded-full border-primary border-2 text-center cursor-pointer transition-all duration-500 hover:bg-black hover:scale-105 hover:border-2 group'>
+                        <ArrowRight size={40} />
                     </div>
                 </div>
             </div>
